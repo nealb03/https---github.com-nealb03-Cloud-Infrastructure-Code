2c65fd9c6f06dcd527e032d6921c2eabd4702d0d
Brent Neal <bneal5@twc.com>
2025-09-05 10:10:39 -0400

    Copy Project1 and Project2 folders from Project3 branch to main

---
+++ Project1/Controllers/Project1Controller.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Project1.Models;
+using Project1.Services;
+using System.Collections.Generic;
+
+namespace Project1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Project1Controller : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<Project>> GetAll()
+        {
+            var projects = ProjectService.GetAll();
+            return Ok(projects);
+        }
+    }
+}
---
+++ Project1/Services/ProjectService.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Project1.Models;
+
+namespace Project1.Services
+{
+    public static class ProjectService
+    {
+        // Returns a sample list of projects
+        public static List<Project> GetAll()
+        {
+            return new List<Project>
+            {
+                new Project { Id = 1, Name = "Project One", Description = "First project" },
+                new Project { Id = 2, Name = "Project Two", Description = "Second project" }
+            };
+        }
+    }
+}
